178c8b0396108b63eba6ae937cd1bd9dc09b44db
Alfred Ashur Oshana <aloshana@students.uni-mainz.de>
2022-07-07 09:33:23 +0200

    added terraformscript

---
+++ aws-terraform-deep-learning - Kopie/vars.tf
@@ -0,0 +1,19 @@
+variable "availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "contact" {
+  default = "blake.cannon@caprinomics.com"
+}
+
+variable "environment" {
+  default = "TEST"
+}
+
+variable "instance_type" {
+  default = "p2.xlarge"
+}
+
+variable "service" {
+  default = "jupyter"
+}
