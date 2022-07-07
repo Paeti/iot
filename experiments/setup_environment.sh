5a7436d28f7c6103cc4a3efa4601287140d37242
Ubuntu <ubuntu@ip-172-31-35-182.eu-central-1.compute.internal>
2022-07-07 11:14:59 +0000

    experiments to get installation running

--- experiments/setup_environment.sh
+++ experiments/setup_environment.sh
@@ -8,4 +8,15 @@ sudo apt install python3.8 -y
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 20
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 20
 
+sudo apt remove python3-apt -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+sudo apt install python3-apt -y
+
+export PATH="/home/ubuntu/.local/bin:$PATH"
+
+sudo apt install pyton3-pip -y
 python3 -m pip install --user pipenv
+
+sudo rm /usr/local/cuda
+sudo ln -s /usr/local/cuda-11.0 /usr/local/cuda
