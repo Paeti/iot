7395696793c48dc376229e71c1c74080c1f7fcf2
Patrick Reckeweg <patrick.reckeweg@googlemail.com>
2022-07-13 13:19:54 +0200

    try to fix install script for cpu image

--- experiments/setup_environment_cpu.sh
+++ experiments/setup_environment_cpu.sh
@@ -20,9 +20,10 @@ apt install python3.9 python3.9-dev -y
 update-alternatives --install /usr/bin/python python /usr/bin/python3.9 20
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 20
 
+apt-get install python3.9-distutils -y
 apt install python3-pip -y
 
-apt remove python3-apt -y
-apt autoremove -y
-apt autoclean -y
-apt install python3-apt -y
+#apt remove python3-apt -y
+#apt autoremove -y
+#apt autoclean -y
+#apt install python3-apt -y
